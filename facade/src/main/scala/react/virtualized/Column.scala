a1bbc52ba54466041ae74403756d746e46a05fbd
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2017-10-16 12:47:26 -0300

    Add some column properties and tests

--- facade/src/main/scala/react/virtualized/Column.scala
+++ facade/src/main/scala/react/virtualized/Column.scala
@@ -4,6 +4,7 @@ package virtualized
 
 import japgolly.scalajs.react._
 import scala.scalajs.js
+import js.JSConverters._
 import japgolly.scalajs.react.raw.{JsNumber, ReactNode}
 import scala.scalajs.js.annotation.{JSImport, ScalaJSDefined}
 
@@ -15,9 +16,18 @@ object Column {
 
   @ScalaJSDefined
   trait CellDataParameter extends js.Object {
-    val columnData: js.Any
-    val dataKey: String
-    val rowData: js.Any
+    var columnData: js.Any
+    var dataKey: String
+    var rowData: js.Any
+  }
+  object CellDataParameter {
+    def apply(columnData: js.Any, dataKey: String, rowData: js.Any): CellDataParameter = {
+      val p = (new js.Object).asInstanceOf[CellDataParameter]
+      p.columnData = columnData
+      p.dataKey = dataKey
+      p.rowData = rowData
+      p
+    }
   }
   type CellDataGetter = js.Function1[CellDataParameter, Any]
 
@@ -105,10 +115,17 @@ object Column {
     var width: JsNumber = js.native
   }
 
-  def props(width: Int, dataKey: String): Props = {
+  def props(
+    width: Int,
+    dataKey: String,
+    ariaLabel: js.UndefOr[String] = js.undefined,
+    cellDataGetter: Option[CellDataGetter] = None
+  ): Props = {
     val p = (new js.Object).asInstanceOf[Props]
     p.width = width
     p.dataKey = dataKey
+    p.`aria-label` = ariaLabel
+    p.cellDataGetter = cellDataGetter.orUndefined
     p
   }
 
--- facade/src/test/scala/react/virtualized/ColumnSpec.scala
+++ facade/src/test/scala/react/virtualized/ColumnSpec.scala
@@ -3,11 +3,26 @@ package react
 package virtualized
 
 import org.scalatest.{FlatSpec, Matchers}
+import Column._
+import scala.scalajs.js
+import js.JSConverters._
 
 class ColumnSpec extends FlatSpec with Matchers {
   "Column" should
-    "be created with minimal properties" in {
-      Column(Column.props(200, "key"))
+    "require width and dataKey" in {
+      Column(Column.props(200, "key")).props.width should be (200)
+      Column(Column.props(200, "key")).props.dataKey should be ("key")
+    }
+    it should "support ariaLabel" in {
+      Column(Column.props(200, "key")).props.`aria-label` should be(())
+      Column(Column.props(200, "key", ariaLabel = "Label")).props.`aria-label` should be("Label")
+    }
+    it should "have a default cellDataGetter" in {
+      val dataMap = Map("key" -> 1, "b" -> 2).toJSDictionary
+      Column(Column.props(200, "key")).props.cellDataGetter.toOption.map(_(CellDataParameter("col", "key", dataMap))) should contain(1)
+    }
+    it should "support cellDataGetter" in {
+      Column(Column.props(200, "key", cellDataGetter = Some((x: CellDataParameter) => "abc"))).props.cellDataGetter.toOption.map(_(CellDataParameter("col", "key", "row"))) should contain("abc")
     }
 
 }
