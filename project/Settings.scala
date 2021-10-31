c2faa322575727355caf3cf1aafac32b40122a9a
Raúl Piaggio <rpiaggio@gemini.edu>
2021-10-31 15:18:26 -0300

    Bump versions.

--- project/Settings.scala
+++ project/Settings.scala
@@ -2,9 +2,10 @@ object Settings {
   object LibraryVersions {
     val reactJS          = "17.0.2"
     val reactVirtualized = "9.21.1"
-    val scalaJsReact     = "2.0.0-RC3"
-    val react            = "0.13.1"
-    val cats             = "2.6.1"
-    val munit            = "0.7.29"
+
+    val scalaJsReact = "2.0.0-RC5"
+    val react        = "0.14.6"
+    val cats         = "2.6.1"
+    val munit        = "0.7.29"
   }
 }
