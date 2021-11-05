a209dc10f3992d02a607aef8d658320b7ed5aaac
Carlos Quiroz <3615303+cquiroz@users.noreply.github.com>
2021-11-05 10:01:14 -0300

    Update lis

--- project/Settings.scala
+++ project/Settings.scala
@@ -3,8 +3,8 @@ object Settings {
     val reactJS          = "17.0.2"
     val reactVirtualized = "9.21.1"
 
-    val scalaJsReact = "2.0.0-RC5"
-    val react        = "0.14.6"
+    val scalaJsReact = "2.0.0"
+    val react        = "0.14.7"
     val cats         = "2.6.1"
     val munit        = "0.7.29"
   }
