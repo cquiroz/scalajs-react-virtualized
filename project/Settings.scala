d73db08e0bc744cb71526cf6e660c7de13e19c73
Scala Steward <me@scala-steward.org>
2022-01-21 06:16:15 +0100

    Update scalajs-react:core, ... to 2.0.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -3,7 +3,7 @@ object Settings {
     val reactJS          = "17.0.2"
     val reactVirtualized = "9.21.1"
 
-    val scalaJsReact = "2.0.0"
+    val scalaJsReact = "2.0.1"
     val react        = "0.14.7"
     val cats         = "2.7.0"
     val munit        = "0.7.29"
